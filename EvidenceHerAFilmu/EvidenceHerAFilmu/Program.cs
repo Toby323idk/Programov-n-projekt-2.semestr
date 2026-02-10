46190171882dd43fbdf36261b51d6a6677d8671f
Tobiáš Pořt <tobigun1@gmail.com>
2026-02-10 20:27:50 +0100

    doupravení kodu a přidání komentů - úprava funkce s menu

--- EvidenceHerAFilmu/EvidenceHerAFilmu/Program.cs
+++ EvidenceHerAFilmu/EvidenceHerAFilmu/Program.cs
@@ -13,62 +13,60 @@ namespace EvidenceHerAFilmu
         static int ShowMenu() // Funkce pro zobrazení Mmenu
         {
             // vytvoření proměnné, do které uložím výběr uživatele
-            int vyber = 0;
+            int vyber = -1;
             while (true)
             {
-                while (true)
+                // Vypsání na obrazovku
+                Console.WriteLine("Hry");
+                Console.WriteLine("1.Zobrazit seznam");
+
+                // změna barvy následujícího vypsání na obrazovku
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("2.Přidat");
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("3.Odebrat\n\n");
+
+                Console.ForegroundColor = ConsoleColor.White;
+
+                // Vypsání na obrazovku
+                Console.WriteLine("Filmy");
+                Console.WriteLine("4.Zobrazit seznam");
+
+                // změna barvy následujícího vypsání na obrazovku
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("5.Přidat");
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("6.Odebrat");
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("\n\n0.Ukončení\n\n");
+
+                /*
+                    kód v bloku try se provede a pokud proběhne bez problému nic se nestane, pokud dojde k chybě provede se block catch
+                */
+                try
                 {
-                    // smazání konzole
-                    Console.Clear();
-
-                    // Vypsání na obrazovku
-                    Console.WriteLine("Hry");
-                    Console.WriteLine("1.Zobrazit seznam");
-
-                    // změna barvy následujícího vypsání na obrazovku
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("2.Přidat");
-
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("3.Odebrat\n\n");
-
-                    Console.ForegroundColor = ConsoleColor.White;
-
-                    // Vypsání na obrazovku
-                    Console.WriteLine("Filmy");
-                    Console.WriteLine("4.Zobrazit seznam");
-
-                    // změna barvy následujícího vypsání na obrazovku
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("5.Přidat");
-
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("6.Odebrat");
-
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("\n\n0.Ukončení\n\n");
-
                     /*
-                        kód v bloku try se provede a pokud proběhne bez problému nic se nestane, pokud dojde k chybě provede se block catch
+                        Uložení textu z konzole do proměnné
+                        vrácená hodnota z Console.ReadLin() = string, int.Parse to převede na int (celé číslo)
                     */
-                    try
-                    {
-                        /*
-                            Uložení textu z konzole do proměnné
-                            vrácená hodnota z Console.ReadLin() = string, int.Parse to převede na string
-                        */
-                        vyber = int.Parse(Console.ReadLine());
-                        break;
-                    }
-                    catch
-                    {
-                        Console.WriteLine("Vyberte prosím číslo z menu");
-                    }
-
+                    vyber = int.Parse(Console.ReadLine());
                 }
-                if(vyber < 7 && vyber > -1)
+                catch // tento blok se provede pouze pokud uživatel nezadá číslo
+                {
+                    vyber = -1; // proměnná výběru uživatele se nastaví na neplatnou hodnotu
+                }
+                // kontrola zda je hodnoa výběru zadaná v intervalu <0;6>
+                if (vyber < 7 && vyber > -1)
                 {
                     return vyber;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("\nVyberte prosím číslo mezi 0-6\n");
                 }
             }
         }
