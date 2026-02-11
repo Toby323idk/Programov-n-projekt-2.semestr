2770948440017103fe67710178ac0c2eb2d88c9a
Tobiáš Pořt <tobigun1@gmail.com>
2026-02-11 18:04:46 +0100

    příprava switchcasu v hlavní funkci

--- EvidenceHerAFilmu/EvidenceHerAFilmu/Program.cs
+++ EvidenceHerAFilmu/EvidenceHerAFilmu/Program.cs
@@ -6,9 +6,21 @@ namespace EvidenceHerAFilmu
     {
         static void Main(string[] args)
         {
-            int cislo = ShowMenu();
-            Console.Clear();
-            Console.WriteLine(cislo);
+            switch (ShowMenu())
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+            }
         }
         static int ShowMenu() // Funkce pro zobrazení Mmenu
         {
