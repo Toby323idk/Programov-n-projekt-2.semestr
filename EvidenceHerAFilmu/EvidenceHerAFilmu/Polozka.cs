76168d06991958df12ea655e46fd7b3e97941c0d
Tobiáš Pořt <tobigun1@gmail.com>
2026-02-11 18:29:52 +0100

    Nakonec jsem se rozhodl třídu film a hry založit na třídě položka

--- EvidenceHerAFilmu/EvidenceHerAFilmu/Polozka.cs
+++ EvidenceHerAFilmu/EvidenceHerAFilmu/Polozka.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace EvidenceHerAFilmu
 {
-    internal class Film
+    internal class Polozka // tato třída bude rodičem třídy hry a filmy
     {
         // vytvoření jednotlivých atributů dané třídy
         public string Nazev { get; private set; } // public get = hodnota se dá získat všude v namespacu, private set = hodnota se dá změnit pouze uvnitř třídy, díky to mu získáváme kontrolu nad změnou hodnot
@@ -12,12 +12,18 @@ namespace EvidenceHerAFilmu
         public float Hodnoceni { get; private set; }
         public string Zanr { get; private set; }
 
-        public Film(string nazev, int rokVydani, float hodnoceni, string zanr) // konstruktor, stejnojmená funkce, která se automaticky vyvolává při zakládání instance objektu, dojde ke vložení hodnot do objektu
+        public Polozka(string nazev, int rokVydani, float hodnoceni, string zanr) // konstruktor, stejnojmená funkce, která se automaticky vyvolává při zakládání instance objektu, dojde ke vložení hodnot do objektu
         {
             Nazev = nazev;
             RokVydani = rokVydani;
             Hodnoceni = (float)Math.Round(hodnoceni, 1); // zaokrohlení Hodnocení na jednodesetinné místo
             Zanr = zanr;
+        }
+
+        // Klíčové slovo virtual dovolí dětem metodu přepsat
+        public virtual void VypisInfo()
+        {
+            Console.WriteLine($"{Nazev} ({RokVydani}) - Hodnocení: {Hodnoceni}/10, Žánr: {Zanr}");
         }
     }
 }
